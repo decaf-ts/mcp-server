625606d342d4af8cc00fbe98f980d2e4914b14b0
tvenceslau <venceslau.tiago@gmail.com>
2025-10-25 12:49:43 +0100

    minor - specify generated first draft-no-ci - after release preparation

--- src/modules/decoration/resources/index.ts
+++ src/modules/decoration/resources/index.ts
@@ -3,7 +3,7 @@ export const resources = [
     "id": "decoration.repo",
     "name": "decoration repository",
     "description": "Source repository",
-    "uri": "file:///home/tvenceslau/local-workspace/decaf-ts/mcp-server/src/modules/decoration",
-    "absolutePath": "/home/tvenceslau/local-workspace/decaf-ts/mcp-server/src/modules/decoration"
+    "uri": "file:///tmp/mcp-gen-y0kwu6/decoration",
+    "absolutePath": "/tmp/mcp-gen-y0kwu6/decoration"
   }
 ] as const;
